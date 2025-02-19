3c329df87aeaf5282bde76a84d9d1d26e536c4aa
karthik-2301050111 <2301050111@kluniversity.in>
2025-02-19 12:07:05 +0530

    initail commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let x = document.getElementById("img");
+
+ function changeMoon() {
+            x.src = "./m.png";
+            document.body.style.background = "black"
+ }
+ function changeSun() {
+            x.src = "./s.png";
+           document.body.style.background = "white"
+ }
